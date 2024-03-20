f2dd7ac7f3e17e9b75a22e4520d60b73e286267b
a1onedeveloper <141642705+a1onedeveloper@users.noreply.github.com>
2024-03-20 14:12:49 +0500

    sirojiddin

--- home/urls.py
+++ home/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('account/', include('account.urls'))
+    path('account/', include('account.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
+    path('', index_view, name='index_url')
 ]
