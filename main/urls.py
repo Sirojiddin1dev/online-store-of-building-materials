a9c6f11ad52e5c9150646438c908b0fba91f2536
orufbek01 <vohobovmansurbek0@gmial.com>
2024-03-21 16:55:17 +0300

    Orufbek

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('index/', index_view, name='index_url')
+    path('index/', index_view, name='index_url'),
+    path('blog/', blog_view, name='blog_url'),
+    path('sing-blog/<int:pk>/', single_blog_view, name='single_blog_url')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
-
+from .models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index_view(request):
     return render(request, 'index.html')
+
+
+def PegenatorPage(List, num, request):
+    paginator = Paginator(List, num)
+    pages = request.GET.get('page')
+    try:
+        list = paginator.page(pages)
+    except PageNotAnInteger:
+        list = paginator.page(1)
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)
+    return list
+
+
+def blog_view(request):
+    blog = Blog.objects.all()
+    context = {
+        'a': PegenatorPage(blog, 9, request),
+        'info': Info.objects.last()
+    }
+    return render(request, 'blog.html', context)
+
+
+def single_blog_view(request, pk):
+    blog = Blog.objects.get(pk=pk)
+    context = {
+        'single_blog': blog,
+        'blog': Blog.objects.all().order_by('-id')[:2]
+    }
+    return render(request, 'blog-details.html', context)
