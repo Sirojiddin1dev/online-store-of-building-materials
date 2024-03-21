416f100826e7b2e8cd9b7e76b4437b9162a7f101
orufbek01 <vohobovmansurbek0@gmial.com>
2024-03-21 17:05:04 +0300

    Orufbek

--- main/views.py
+++ main/views.py
@@ -31,6 +31,7 @@ def single_blog_view(request, pk):
     blog = Blog.objects.get(pk=pk)
     context = {
         'single_blog': blog,
-        'blog': Blog.objects.all().order_by('-id')[:2]
+        'blog': Blog.objects.all().order_by('-id')[:2],
+        'info': Info.objects.last()
     }
     return render(request, 'blog-details.html', context)
