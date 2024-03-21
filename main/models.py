cd1ae2b70c710a9b6521449dba6c665a377e1b7b
orufbek01 <vohobovmansurbek0@gmial.com>
2024-03-21 15:11:51 +0300

    Orufbek

--- main/models.py
+++ main/models.py
@@ -1,45 +1,31 @@
 from django.db import models
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=25)
-
-    def __str__(self):
-        return self.name
-
-
-class News(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    description = models.TextField(null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='news_photo/')
-    date = models.DateField(auto_now=True)
-    time = models.TimeField(auto_now=True)
-    tag = models.ManyToManyField('Tag')
-    view = models.IntegerField(default=0)
-    video = models.CharField(max_length=255, null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=25)
-
-    def __str__(self):
-        return self.name
-
-
-class Newsletter(models.Model):
-    email = models.CharField(max_length=55)
-
-    def __str__(self):
-        return self.email
+from django.core.validators import RegexValidator
 
 
 class Info(models.Model):
-    logo = models.ImageField(upload_to='logo/')
-    location = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='info_img/')
+    address = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone_number = models.CharField( max_length=17, validators=[
+        RegexValidator(
+            regex=r'^\+?1?\d{9,15}$',
+            message="Telefon raqamingizni to'g'ri ko'rsating.",
+            code = "Telefon raqa, xato"
+        )
+    ])
+    facebook = models.CharField(max_length=255)
+    youtube = models.CharField(max_length=255)
+    twitter = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.facebook
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.subject
