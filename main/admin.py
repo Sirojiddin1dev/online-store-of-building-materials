05f3349088d612872f789d38f1fac4fe4dc2fbcb
orufbek01 <vohobovmansurbek0@gmial.com>
2024-03-21 15:33:46 +0300

    Orufbek

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import *
 
 
+admin.site.register(Info)
+admin.site.register(Category)
+admin.site.register(Contact)
+admin.site.register(Blog)
+admin.site.register(Image)
 admin.site.register(Tag)
