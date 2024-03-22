5c02d7ddb16028a8a3552fa986f1d65f27d76925
Muhammadabdullox Kozimjonov <kozimjonova00@gmail.com>
2024-03-22 16:15:03 +0500

    Abdullox

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 from .models import *
 
+
+def banner_view(request):
+    product = Products.objects.filter(is_banner=True)
+    context = {
+        'product': Products.objects.all().order_by('-id')[:2]
+    }
+    return render(request, 'product.html', context)
+
+
+def product_view(request):
+    context ={
+        "products":Products.objects.all().order_by('-id')[:2]
+    }
+    return render(request, 'product.html', context)
+
+
+def product2_view(request):
+    context={
+        'products':Products.objects.all().order_by('-id')[:4]
+    }
+    return render(request, 'product.html', context)
+
+
+def product3_view(request):
+    context={
+        'products':Products.objects.all().order_by('-id')[:2]
+    }
+    return render(request, 'product.html', context)
