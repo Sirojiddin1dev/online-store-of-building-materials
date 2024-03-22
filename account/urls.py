222eafd3eccfd3ea20e1f9b7f047ba44a75d7265
Muhammadabdullox Kozimjonov <kozimjonova00@gmail.com>
2024-03-22 12:48:01 +0500

    Abdullox

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/', create_user_view, name='register_url'),
     path('', login_view, name='login_url'),
     path('my-profile/', my_profile_view, name='my_profile_url'),
+    path('account-details/', edit_user_view, name='account_details_url'),
     path('logout/', logout_view, name='logout_url'),
 ]
