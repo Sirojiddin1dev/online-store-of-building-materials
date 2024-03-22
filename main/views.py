1f7d7c8506ea1dfbf341e8526008f008e813b60a
orufbek01 <vohobovmansurbek0@gmial.com>
2024-03-22 11:48:13 +0300

    Orufbek

--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import *
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index_view(request):
     context = {
@@ -9,22 +8,9 @@ def index_view(request):
     return render(request, 'index.html', context)
 
 
-def PegenatorPage(List, num, request):
-    paginator = Paginator(List, num)
-    pages = request.GET.get('page')
-    try:
-        list = paginator.page(pages)
-    except PageNotAnInteger:
-        list = paginator.page(1)
-    except EmptyPage:
-        list = paginator.page(paginator.num_pages)
-    return list
-
-
 def blog_view(request):
-    blog = Blog.objects.all()
     context = {
-        'a': PegenatorPage(blog, 9, request),
+        'blog': Blog.objects.all().order_by('-id')[:9],
         'info': Info.objects.last()
     }
     return render(request, 'blog.html', context)
