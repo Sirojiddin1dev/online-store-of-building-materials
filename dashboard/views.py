c51badf4190eee31b97e5c9b52820797758ea143
Muhammadabdullox Kozimjonov <kozimjonova00@gmail.com>
2024-03-23 16:03:49 +0500

    Abdullox

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,51 +3,21 @@ from .models import *
 
 
 def banner_view(request):
-    product = Products.objects.filter(is_banner=True).order_by('-id')[:2]
-    context = {
-        'banner': product
-    }
-    return render(request, 'product.html', context)
-
-
-def product1_view(request):
-    product = Products.objects.filter(featured_product = True).order_by('-id')[:2]
-    context = {
-        "product1": product
-    }
-    return render(request, 'product.html', context)
-
-
-def product2_view(request):
-    product = Products.objects.all().order_by('-view')[:4]
-    context = {
-        'product2': product
-    }
-    return render(request, 'product.html', context)
-
-
-def product3_view(request):
-    product = Products.objects.filter(is_advert = True)[:3]
-    context = {
-        'product3': product
-    }
-    return render(request, 'product.html', context)
-
-
-def product4_view(request):
+    banner = Products.objects.filter(is_banner=True).order_by('-id')[:2]
+    product1 = Products.objects.filter(featured_product = True).order_by('-id')[:2]
+    product2 = Products.objects.all().order_by('-view')[:4]
+    product3 = Products.objects.filter(is_advert=True)[:3]
     product = Products.objects.all().order_by('-new_product')[:12]
-    context = {
-        'product4': product
-    }
-    return render(request, 'product.html', context)
-
-
-def product5_view(request):
     product = Products.objects.all().order_by('-product_info')[:6]
     context = {
-        'product5':product
+        'banner': banner,
+        "product1": product1,
+        'product2': product2,
+        'product3': product3,
+        'product4': product4,
+        'product5': product5,
     }
-    return render(request, 'product.html', context)
+    return render(request, 'index.html', context)
 
 
 def shopping_view(request):
