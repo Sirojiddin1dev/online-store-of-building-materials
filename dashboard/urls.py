6410ab212c601cdcd9c1b57baebc34ef09d67d74
Muhammadabdullox Kozimjonov <kozimjonova00@gmail.com>
2024-03-23 16:11:57 +0500

    Abdullox

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,11 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shop/', views.shopping_view, name = "shop_url"),
-    path('banner/', views.banner_view, name = "banner_url"),
-    path('product1/', views.product1_view, name = "product1_url"),
-    path('banner/', views.product2_view, name = "product2_url"),
-    path('banner/', views.product3_view, name = "product3_view"),
-    path('banner/', views.product4_view, name = "product4_url"),
-    path('banner/', views.product5_view, name = "product5_view"),
+    path('banner/', views.banner_view, name = "index_url"),
 ]
